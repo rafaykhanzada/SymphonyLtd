b8f8ed232552efb3605b0e1ed74ab916cc37ecdf
Rafay Khan <rafaykhan3012@gmail.com>
2021-04-06 23:57:36 +0500

    Add project files.

---
+++ SymphonyLtd/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SymphonyLtd
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
