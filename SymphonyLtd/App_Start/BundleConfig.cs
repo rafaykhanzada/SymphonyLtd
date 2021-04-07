fc9149cca49d8f89db39d0c69b12fddae31e43f6
Rafay Khan <rafaykhan3012@gmail.com>
2021-04-07 20:53:13 +0500

    Public

--- SymphonyLtd/App_Start/BundleConfig.cs
+++ SymphonyLtd/App_Start/BundleConfig.cs
@@ -25,6 +25,28 @@ namespace SymphonyLtd
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            //Custom Bundling Admin Side
+            bundles.Add(new StyleBundle("~/Admin/css").Include(
+                    "~/App_Assets/css/app.min.css",
+                    "~/App_Assets/css/style.css",
+                    "~/App_Assets/css/styles/all-themes.css"
+                    ));
+            bundles.Add(new ScriptBundle("~/Admin/script").Include(
+                        "~/App_Assets/Admin/js/app.min.js",
+                        "~/App_Assets/Admin/js/admin.js"));
+            //Custom Bundling Public Side
+            bundles.Add(new StyleBundle("~/Public/script").Include(
+                        "~/App_Assets/Admin/js/app.min.js",
+                        "~/App_Assets/Admin/js/admin.js"));
+            bundles.Add(new ScriptBundle("~/Public/css").Include(
+                  "~/App_Assets/Public/css/bootstrap.bundle.min.js",
+                  "~/App_Assets/Public/css/tiny-slider.js",
+                  "~/App_Assets/Public/css/tobii.min.js",
+                  "~/App_Assets/Public/css/feather.min.js",
+                  "~/App_Assets/Public/css/switcher.js",
+                  "~/App_Assets/Public/css/plugins.init.js",
+                  "~/App_Assets/Public/css/app.js"));
+           
         }
     }
 }
